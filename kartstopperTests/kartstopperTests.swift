812dad8d55a05f16ed71dbbdfe3280ecc68f48f3
Ashish Brahma <ashish.brahma@outlook.com>
2025-09-05 20:28:58 +0530

    Initial Commit

---
+++ kartstopperTests/kartstopperTests.swift
@@ -0,0 +1,17 @@
+//
+//  kartstopperTests.swift
+//  kartstopperTests
+//
+//  Created by Ashish Brahma on 05/09/25.
+//
+
+import Testing
+@testable import kartstopper
+
+struct kartstopperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
